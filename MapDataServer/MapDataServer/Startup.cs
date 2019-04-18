daf684566183ee4b7de14dc5776c153908df2519
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-04-17 20:20:03 -0700

    Got some sample MySQL code working in C#!

--- MapDataServer/MapDataServer/Startup.cs
+++ MapDataServer/MapDataServer/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
 
 namespace MapDataServer
 {
@@ -26,6 +27,31 @@ namespace MapDataServer
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            DbConnect();
+        }
+
+        async void DbConnect()
+        {
+            MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(
+                "Data Source='mapdataserverdb.ckzhqptufd8c.us-east-2.rds.amazonaws.com';Port=3306;Database='map_data';UID='admin';PWD='hijklmnop';");
+            await connection.OpenAsync();
+            var database = connection.Database;
+            //using (var command = new MySqlCommand("CREATE TABLE IF NOT EXISTS map_regions(long_region SMALLINT, lat_region SMALLINT, PRIMARY KEY (long_region, lat_region));", connection))
+            //{
+            //    var result = await command.ExecuteNonQueryAsync();
+            //}
+            //using (var command = new MySqlCommand("INSERT INTO map_regions VALUES(0, 0);", connection))
+            //{
+            //    var result = await command.ExecuteNonQueryAsync();
+            //}
+            using (var command = new MySqlCommand("SELECT COUNT(CASE WHEN long_region = 0 AND lat_region = 1 THEN 1 END) FROM map_regions;", connection))
+            {
+                var result = await command.ExecuteScalarAsync();
+                
+            }
+            await connection.CloseAsync();
+            connection.Dispose();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
