f3c48f8f40f4021e717845d479107914422b9e93
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-04-19 19:02:19 -0700

    Begin to make wrappers for MySql.Data classes.

---
+++ MapDataServer/MapDataServer/Services/IDatabaseConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapDataServer.Services
+{
+    public interface IDatabaseConnection : IDisposable
+    {
+        Task OpenAsync();
+        Task CloseAsync();
+    }
+}
