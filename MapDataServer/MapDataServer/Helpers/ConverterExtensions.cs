b1250c58f0d230963d66146640f0bab0a4ef2fd1
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-05-12 21:41:03 -0700

    Created GeoPoint class.

---
+++ MapDataServer/MapDataServer/Helpers/ConverterExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MapDataServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapDataServer.Helpers
+{
+    public static class ConverterExtensions
+    {
+        public static (double, double) ToTuple(this GeoPoint point) => (point.Longitude, point.Latitude);
+
+        public static GeoPoint ToGeoPoint(this (double, double) tuple) => new GeoPoint(tuple.Item1, tuple.Item2);
+
+        public static GeoPoint GetPoint(this MapNode node)
+        {
+            return new GeoPoint(node.Longitude, node.Latitude);
+        }
+    }
+}
---
+++ MapDataServer/MapDataServer/Models/GeoPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapDataServer.Models
+{
+    public struct GeoPoint
+    {
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+        public GeoPoint(double longitude, double latitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double DistanceTo(GeoPoint other) => Math.Sqrt((this.Longitude - other.Longitude) * (this.Longitude - other.Longitude) + (this.Latitude - other.Latitude) * (this.Latitude - other.Latitude));
+
+        public static double Distance(GeoPoint p1, GeoPoint p2) => p1.DistanceTo(p2);
+    }
+}
