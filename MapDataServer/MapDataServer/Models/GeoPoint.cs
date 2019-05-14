88094df5763ab34cd55c6b9c74dc524aba2f0de0
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-05-14 08:00:51 -0700

    Added a proper algorithm to find the distance between geo points.

--- MapDataServer/MapDataServer/Models/GeoPoint.cs
+++ MapDataServer/MapDataServer/Models/GeoPoint.cs
@@ -16,8 +16,25 @@ namespace MapDataServer.Models
             Longitude = longitude;
         }
 
-        public double DistanceTo(GeoPoint other) => Math.Sqrt((this.Longitude - other.Longitude) * (this.Longitude - other.Longitude) + (this.Latitude - other.Latitude) * (this.Latitude - other.Latitude));
+        // From https://stackoverflow.com/questions/6544286/calculate-distance-of-two-geo-points-in-km-c-sharp
+        public double DistanceTo(GeoPoint other)
+        {
+            var lat1 = Latitude * Math.PI / 180;
+            var lat2 = other.Latitude * Math.PI / 180;
+            var lon1 = Longitude * Math.PI / 180;
+            var lon2 = other.Longitude * Math.PI / 180;
 
-        public static double Distance(GeoPoint p1, GeoPoint p2) => p1.DistanceTo(p2);
+
+            double r = 6371; // km
+            double dLat = lat2 - lat1;
+            double dLon = lon2 - lon1;
+
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) + Math.Sin(dLon / 2) * Math.Sin(dLon / 2) * Math.Cos(lat1) * Math.Cos(lat2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = r * c;
+
+            return d;
+        }
+
     }
 }
