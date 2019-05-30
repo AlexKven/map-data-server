42ae318bac3fd1fe258538d3eb18d71420653439
Alexander Kvenvolden <kvenclassical@gmail.com>
2019-05-30 08:15:31 -0700

    Start working on MapStep, to store calculated route details in the database.

---
+++ MapDataServer/MapDataServer/Models/MapStep.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapDataServer.Models
+{
+    public class MapStep
+    {
+        public long TripId { get; set; }
+
+        public long NodeId { get; set; }
+
+        public long? LinkId { get; set; }
+
+        public ushort? WayIndex { get; set; }
+
+        public short? WayStepCount { get; set; }
+
+        public uint ItemIndex { get; set; }
+
+        public double AvgSpeed { get; set; }
+
+        public object SpeedDistribution { get; set; }
+    }
+}
