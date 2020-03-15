e55cbaff19b81043e9f2e9ab3ab810f1602157e3
AlexKven <kvenclassical@gmail.com>
2020-03-15 11:27:28 -0700

    Added better ActivitySummary endpoint with a client/server model class.

---
+++ MapDataServer/ClientServerShared/Models/ActivitySummary.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapDataServer.Models
+{
+    public class ActivitySummary
+    {
+        public ActivitySummary()
+        {
+            Distances = new uint[HovTypesCount];
+            Times = new TimeSpan[HovTypesCount];
+            Counts = new int[HovTypesCount];
+        }
+
+        public static int HovTypesCount => Enum.GetValues(typeof(HovStatus)).Length;
+        public uint[] Distances { get; set; }
+        public TimeSpan[] Times { get; set; }
+        public int[] Counts { get; set; }
+        public int UnprocessedCount { get; set; }
+        public int InsignificantCount { get; set; }
+
+        public void AddTrip(Trip trip, PreprocessedTrip preprocessed)
+        {
+            if (preprocessed == null)
+            {
+                UnprocessedCount++;
+                return;
+            }
+            if (preprocessed.DistanceMeters == 0)
+            {
+                InsignificantCount++;
+                return;
+            }
+            var hovStatus = trip.HovStatus;
+            Distances[(int)hovStatus] += preprocessed.DistanceMeters;
+            Times[(int)hovStatus] += (preprocessed.ActualEndTime - preprocessed.ActualStartTime);
+            Counts[(int)hovStatus]++;
+        }
+
+        public void AddSummary(ActivitySummary other)
+        {
+            UnprocessedCount += other.UnprocessedCount;
+            InsignificantCount += other.InsignificantCount;
+            for (int i = 0; i < HovTypesCount; i++)
+            {
+                Distances[i] += other.Distances[i];
+                Times[i] += other.Times[i];
+                Counts[i] += other.Counts[i];
+            }
+        }
+
+        public override string ToString()
+        {
+            var totalDistance = Distances.Sum(val => val);
+            var totalTime = TimeSpan.FromMilliseconds(Times.Sum(val => val.TotalMilliseconds));
+            var totalCount = Counts.Sum();
+
+            var resultBuilder = new StringBuilder();
+
+            resultBuilder.AppendLine($"Total distance: {totalDistance} meters");
+            resultBuilder.AppendLine($"Total time: {totalTime}");
+            resultBuilder.AppendLine($"Total trips: {totalCount}");
+            resultBuilder.AppendLine();
+
+            for (int i = 0; i < HovTypesCount; i++)
+            {
+
+                var distance = Distances[i];
+                var distFraction = (double)distance / (double)totalDistance;
+                var time = Times[i];
+                var timeFraction = time.TotalSeconds / totalTime.TotalSeconds;
+                var count = Counts[i];
+                var countFraction = (double)count / (double)totalCount;
+                resultBuilder.AppendLine($"For {(HovStatus)i}:");
+                resultBuilder.AppendLine($"Distance: {distance} meters ({distFraction.ToString("P")})");
+                resultBuilder.AppendLine($"Time: {time} ({timeFraction.ToString("P")})");
+                resultBuilder.AppendLine($"Trips: {count} ({countFraction.ToString("P")})");
+                resultBuilder.AppendLine();
+            }
+
+            resultBuilder.AppendLine($"{UnprocessedCount} unprocessed trips and {InsignificantCount} zero-length trips not included in summary.");
+
+            return resultBuilder.ToString();
+        }
+    }
+}
--- MapDataServer/MapDataServer/Controllers/TripDataController.cs
+++ MapDataServer/MapDataServer/Controllers/TripDataController.cs
@@ -115,62 +115,38 @@ namespace MapDataServer.Controllers
         }
 
         [HttpGet("activitySummary")]
-        public async Task<ActionResult> ActivitySummary([FromQuery] DateTime startTime, [FromQuery] DateTime endTime)
+        public async Task<ActionResult> ActivitySummary([FromQuery] DateTime startTime, [FromQuery] DateTime endTime, [FromQuery] bool textSummary = false)
         {
             if (startTime > endTime)
                 return BadRequest();
             if (endTime - startTime > TimeSpan.FromDays(366))
                 return BadRequest();
 
-            var hovTypesCount = Enum.GetValues(typeof(HovStatus)).Length;
-            uint[] distances = new uint[hovTypesCount];
-            TimeSpan[] times = new TimeSpan[hovTypesCount];
+            ActivitySummary summary = new ActivitySummary();
 
             DateTime beginTime = DateTime.UtcNow;
             var trips = await (from trip in Database.Trips
                          where trip.StartTime >= startTime &&
                                 trip.EndTime < endTime
                          select trip).ToArrayAsync();
-            var preprocessedTripCount = 0;
 
             foreach (var trip in trips)
             {
+                if (trip.HovStatus <= HovStatus.Motorcycle && await Database.ObaTripLinks.AnyAsync(oba => oba.MapTripId == trip.Id))
+                    trip.HovStatus = HovStatus.Transit;
                 var preprocessed = await Database.PreprocessedTrips.Where(processed => processed.Id == trip.Id).FirstOrDefaultAsync();
-                if (preprocessed == null)
-                    continue;
-                preprocessedTripCount++;
-                var hovStatus = trip.HovStatus;
-                if (hovStatus <= HovStatus.Motorcycle && await Database.ObaTripLinks.AnyAsync(oba => oba.MapTripId == trip.Id))
-                    hovStatus = HovStatus.Transit;
-                distances[(int)hovStatus] += preprocessed.DistanceMeters;
-                times[(int)hovStatus] += (preprocessed.ActualEndTime - preprocessed.ActualStartTime);
+
+                summary.AddTrip(trip, preprocessed);
             }
 
-            var totalDistance = distances.Sum(val => val);
-            var totalTime = TimeSpan.FromMilliseconds(times.Sum(val => val.TotalMilliseconds));
-
-            var resultBuilder = new StringBuilder();
-
-            resultBuilder.AppendLine($"Results from {preprocessedTripCount} out of {trips.Length} trips. Others pending preprocessing.");
-            resultBuilder.AppendLine();
-            resultBuilder.AppendLine($"Total distance: {totalDistance} meters");
-            resultBuilder.AppendLine($"Total time: {totalTime}");
-            resultBuilder.AppendLine();
-
-            for (int i = 0; i < hovTypesCount; i++)
+            if (textSummary)
             {
-                var distance = distances[i];
-                var distFraction = (double)distance / (double)totalDistance;
-                var time = times[i];
-                var timeFraction = time / totalTime;
-                resultBuilder.AppendLine($"For {(HovStatus)i}:");
-                resultBuilder.AppendLine($"Distance: {distance} meters ({distFraction.ToString("P")})");
-                resultBuilder.AppendLine($"Time: {time} ({timeFraction.ToString("P")})");
-                resultBuilder.AppendLine();
+                var resultBuilder = new StringBuilder();
+                resultBuilder.Append(summary.ToString());
+                resultBuilder.AppendLine($"Server time: {DateTime.UtcNow - beginTime}");
+                return new OkObjectResult(resultBuilder.ToString());
             }
-
-            resultBuilder.AppendLine($"Server time: {DateTime.UtcNow - beginTime}");
-            return new OkObjectResult(resultBuilder.ToString());
+            return new OkObjectResult(summary);
         }
 
         [HttpGet("tripsForTimeRange")]
