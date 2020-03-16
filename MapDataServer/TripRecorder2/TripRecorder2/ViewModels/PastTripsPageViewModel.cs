65547de6aaab0531e1d13b4af4bad82f8aa267bd
AlexKven <kvenclassical@gmail.com>
2020-03-15 19:34:25 -0700

    Added trip summaries with pie charts to the trip recorder app.

--- MapDataServer/TripRecorder2/TripRecorder2/ViewModels/PastTripsPageViewModel.cs
+++ MapDataServer/TripRecorder2/TripRecorder2/ViewModels/PastTripsPageViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using MapDataServer.Models;
+using Microcharts;
 using Microsoft.Extensions.Configuration;
 using MvvmHelpers;
 using Newtonsoft.Json;
+using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -55,6 +58,27 @@ namespace TripRecorder2.ViewModels
             }
         }
 
+        private Chart _DistanceChart;
+        public Chart DistanceChart
+        {
+            get => _DistanceChart;
+            private set => SetProperty(ref _DistanceChart, value);
+        }
+
+        private Chart _TimeChart;
+        public Chart TimeChart
+        {
+            get => _TimeChart;
+            private set => SetProperty(ref _TimeChart, value);
+        }
+
+        private Chart _TripsChart;
+        public Chart TripsChart
+        {
+            get => _TripsChart;
+            private set => SetProperty(ref _TripsChart, value);
+        }
+
         private string FormatDate(DateTime dateTime)
         {
             var utc = dateTime.ToUniversalTime();
@@ -84,9 +108,74 @@ namespace TripRecorder2.ViewModels
         private async void Go()
         {
             var summary = await GetSummary();
-            SummaryDownloaded?.Invoke(this, summary);
-        }
 
-        public event EventHandler<ActivitySummary> SummaryDownloaded;
+            var colors = new SKColor[]
+            {
+                // Sov
+                SKColors.Red,
+                // Hov2
+                SKColors.Orange,
+                // Hov3
+                SKColors.DarkOrange,
+                // Motorcycle
+                SKColors.Crimson,
+                // Transit
+                SKColors.Green,
+                // Pedestrian
+                SKColors.Pink,
+                // Bicycle
+                SKColors.Purple,
+                // Streetcar
+                SKColors.Teal,
+                // LightRail
+                SKColors.Blue,
+                // HeavyRail
+                SKColors.DarkBlue
+            };
+
+            var distanceEntries = new List<Microcharts.Entry>();
+            var timeEntries = new List<Microcharts.Entry>();
+            var tripEntries = new List<Microcharts.Entry>();
+
+            var totalDistance = summary.Distances.Sum(v => v);
+            var totalTime = summary.Times.Sum(t => t.TotalSeconds);
+            var totalTrips = summary.Counts.Sum();
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var distance = summary.Distances[i];
+                var distFraction = (double)distance / (double)totalDistance;
+                var time = summary.Times[i];
+                var timeFraction = time.TotalSeconds / totalTime;
+                var count = summary.Counts[i];
+                var countFraction = (double)count / (double)totalTrips;
+
+                distanceEntries.Add(new Microcharts.Entry(distance)
+                {
+                    Color = colors[i],
+                    Label = ((HovStatus)i).ToString(),
+                    TextColor = colors[i],
+                    ValueLabel = $"{distance}m ({distFraction.ToString("P")})"
+                });
+                timeEntries.Add(new Microcharts.Entry((float)time.TotalSeconds)
+                {
+                    Color = colors[i],
+                    Label = ((HovStatus)i).ToString(),
+                    TextColor = colors[i],
+                    ValueLabel = $"{time} ({timeFraction.ToString("P")})"
+                });
+                tripEntries.Add(new Microcharts.Entry(count)
+                {
+                    Color = colors[i],
+                    Label = ((HovStatus)i).ToString(),
+                    TextColor = colors[i],
+                    ValueLabel = $"{count} ({countFraction.ToString("P")})"
+                });
+            }
+
+            DistanceChart = new DonutChart() { Entries = distanceEntries };
+            TimeChart = new DonutChart() { Entries = timeEntries };
+            TripsChart = new DonutChart() { Entries = tripEntries };
+        }
     }
 }
--- MapDataServer/TripRecorder2/TripRecorder2/Views/PastTripsPage.xaml.cs
+++ MapDataServer/TripRecorder2/TripRecorder2/Views/PastTripsPage.xaml.cs
@@ -19,36 +19,35 @@ namespace TripRecorder2.Views
         {
             InitializeComponent();
             var viewModel = AppStartup.Container.Resolve<PastTripsPageViewModel>();
-            viewModel.SummaryDownloaded += ViewModel_SummaryDownloaded;
             BindingContext = viewModel;
         }
 
-        private void ViewModel_SummaryDownloaded(object sender, MapDataServer.Models.ActivitySummary e)
-        {
-            var entries = new[]
-               {
-                new Microcharts.Entry(e.Distances[0])
-                {
-                    Label = "Driving alone",
-                    Color = SKColor.Parse("#266489")
-                },
-                new Microcharts.Entry(e.Distances[1])
-                {
-                Label = "HOV 2",
-                Color = SKColor.Parse("#68B9C0")
-                },
-                new Microcharts.Entry(e.Distances[4])
-                {
-                Label = "Bus",
-                Color = SKColor.Parse("#90D585")
-                }
-            };
-            var chart = new DonutChart()
-            {
-                Entries = entries
-            };
+        //private void ViewModel_SummaryDownloaded(object sender, MapDataServer.Models.ActivitySummary e)
+        //{
+        //    var entries = new[]
+        //       {
+        //        new Microcharts.Entry(e.Distances[0])
+        //        {
+        //            Label = "Driving alone",
+        //            Color = SKColor.Parse("#266489")
+        //        },
+        //        new Microcharts.Entry(e.Distances[1])
+        //        {
+        //        Label = "HOV 2",
+        //        Color = SKColor.Parse("#68B9C0")
+        //        },
+        //        new Microcharts.Entry(e.Distances[4])
+        //        {
+        //        Label = "Bus",
+        //        Color = SKColor.Parse("#90D585")
+        //        }
+        //    };
+        //    var chart = new DonutChart()
+        //    {
+        //        Entries = entries
+        //    };
 
-            ModeChartDistance.Chart = chart;
-        }
+        //    ModeChartDistance.Chart = chart;
+        //}
     }
 }
