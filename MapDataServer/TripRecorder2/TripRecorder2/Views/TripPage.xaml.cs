ae40806fc5eea7222cf61acffe269747f858052d
AlexKven <kvenclassical@gmail.com>
2020-03-19 20:13:31 -0700

    Fixed something I broke, and remove SkiaSharp demo.

--- MapDataServer/TripRecorder2/TripRecorder2/Views/TripPage.xaml.cs
+++ MapDataServer/TripRecorder2/TripRecorder2/Views/TripPage.xaml.cs
@@ -3,7 +3,6 @@ using Plugin.Geolocator;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,7 +25,6 @@ namespace TripRecorder2.Views
             viewModel.PointsUpdated += ViewModel_PointsUpdated;
             BindingContext = viewModel;
             SetLocation();
-            MainMap.TileLayers.Add(TileLayer.FromSyncImage(GetTileImage, 1024));
         }
 
         private async void SetLocation()
@@ -55,43 +53,6 @@ namespace TripRecorder2.Views
                 };
                 MainMap.Pins.Add(pin);
             }
-        }
-
-        private byte[] GetTileImage(int x, int y, int zoom)
-        {
-            var bitmap = new SkiaSharp.SKBitmap(1024, 1024);
-            var canvas = new SkiaSharp.SKCanvas(bitmap);
-            var p1 = LatLonToSquareCoords(47.6153079801, -122.1887000000, x, y, zoom, 1024);
-            var p2 = LatLonToSquareCoords(47.6123821584, -122.199, x, y, zoom, 1024);
-            canvas.Clear();
-            canvas.DrawLine(p1.Item1, p1.Item2, p2.Item1, p2.Item2,
-                new SkiaSharp.SKPaint() { Color = new SkiaSharp.SKColor(255, 0, 0, 175), StrokeWidth = 30 });
-            canvas.Flush();
-            canvas.Dispose();
-
-            using (MemoryStream memStream = new MemoryStream())
-            using (SkiaSharp.SKManagedWStream wstream = new SkiaSharp.SKManagedWStream(memStream))
-            {
-                bitmap.Encode(wstream, SkiaSharp.SKEncodedImageFormat.Png, 100);
-                byte[] data = memStream.ToArray();
-                bitmap.Dispose();
-                return data;
-            }
-        }
-
-        public (float, float) LatLonToSquareCoords(double lat, double lon, int x, int y, int zoom, int tileSize)
-        {
-            // From https://web.archive.org/web/20190603233026/http://troybrant.net/blog/2010/01/mkmapview-and-zoom-levels-a-visual-guide/
-
-            var mercatorOffset = Math.Pow(2, zoom - 1);
-            var mercatorRadius = mercatorOffset / Math.PI;
-
-            var coordX = mercatorOffset + lon * mercatorOffset / 180;
-            var coordY = mercatorOffset - mercatorRadius * Math.Log((1 + Math.Sin(lat * Math.PI / 180)) /
-                (1 - Math.Sin(lat * Math.PI / 180))) / 2.0;
-            var fracX = coordX - x;
-            var fracY = coordY - y;
-            return ((float)fracX * tileSize, (float)fracY * tileSize);
         }
     }
 }
